735a7aab30dc7780893d5e87afe2c02c35a8cc1f
Fred Pene <fredpene@hotmail.co.nz>
2022-08-11 19:11:42 +1200

    set up

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const server = express()
+const hbs = require('express-handlebars')
+const fs = require('fs').promises
+const path = require('path')
+
+// Server configuration
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+// Handlebars configuration
+server.engine('hbs', hbs.engine({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+
+const mainRoute = require('./routes')
+server.use('/puppies', mainRoute)
+
+// Our main home route should go here
+
+server.get('/', async (req, res) => {
+  res.send('testing testing')
+})
+
+module.exports = server
